c3f79e2ebef53276ac079bca0a74cf8666d4e262
tomguitar0802 <78745783+tomguitar0802@users.noreply.github.com>
2023-01-19 21:32:43 +0900

    Update SMA.py

--- SMA.py
+++ SMA.py
@@ -4,16 +4,16 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-Path=st.file_uploader('Excel')
+Path=st.sidebar.file_uploader('Excel')
 skip_rows,time,intensity=3,"Time","Intensity"
 
 if Path is not None:
     Data=pd.ExcelFile(Path)
     Sheet_names=Data.sheet_names
-    ions=st.multiselect("Ions",Sheet_names,Sheet_names)
+    ions=st.sidebar.multiselect("Ions",Sheet_names,Sheet_names)
 
-    span=st.slider("SMA span",1,121,1,2)
-    standardization=st.checkbox('Standardization',value=False)
+    span=st.sidebar.slider("SMA span",1,121,1,2)
+    standardization=st.sidebar.checkbox('Standardization',value=False)
     fig,ax=plt.subplots()
 
     for Sheet_name in ions:
